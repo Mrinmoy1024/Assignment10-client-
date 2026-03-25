45f13a481f1d28a7319d89526374fdc08d31a33e
Mrinmoy1024 <mtex1024@gmail.com>
2026-03-25 18:22:06 +0600

    added necessary packages

---
+++ src/routes/routes.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { createBrowserRouter } from "react-router";
+import MainLayout from "../layout/MainLayout";
+import Home from "../pages/Home";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    children: [
+      {
+        index: true,
+        element: <Home></Home>,
+      },
+    ],
+  },
+]);
